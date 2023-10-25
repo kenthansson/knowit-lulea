57bb4db0437166ec68545a0435fcc020c1f6a361
lisyli <lisa.ylikangas@hotmail.com>
2023-10-25 14:29:07 +0200

    Added table and pagination to display data

--- frontend/src/app/pins-list-page/pins-list-page.component.ts
+++ frontend/src/app/pins-list-page/pins-list-page.component.ts
@@ -1,26 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {PoiService} from "../services/poi.service";
-
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {Poi} from "../models/poi.model";
 
 @Component({
     selector: 'app-pins-list-page',
     templateUrl: './pins-list-page.component.html',
-    styleUrls: ['./pins-list-page.component.css']
+    styleUrls: ['./pins-list-page.component.css'],
+
 })
 
 export class PinsListPageComponent implements OnInit {
 
-    pois: any;
+    pois: Poi[] = [];
+
+    dataSource: MatTableDataSource<Poi>;
+    displayedColumns: string[] = ['name', 'description', 'lat', 'lng'];
+
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
 
     constructor(private poiService: PoiService) {
+        this.dataSource = new MatTableDataSource(this.pois);
     }
 
     ngOnInit(): void {
-        this.poiService.getPois()
-            .subscribe({
-                next: p => this.pois = p,
-                error: e => console.log(e)
-            })
+        this.poiService.getPois().subscribe((data: any) => {
+            this.pois = data;
+            this.dataSource = new MatTableDataSource(this.pois);
+            //  this.dataSource = new MatTableDataSource(data);
+            this.dataSource.paginator = this.paginator;
+        });
     }
 
 
