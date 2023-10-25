ffa0f7db5d1ad87b52f02aeab94aeb2d8eefe244
lisyli <lisa.ylikangas@hotmail.com>
2023-10-25 10:14:20 +0200

    (WIP) Added PinsListPageComponent for displaying pins (name, description and coordinates)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,53 +1,55 @@
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, isDevMode} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { HeaderComponent } from './header/header.component';
-import { MapPageComponent } from './map-page/map-page.component';
-import { PinsListPageComponent } from './pins-list-page/pins-list-page.component';
-import { GoogleMapsModule } from '@angular/google-maps';
-import { HttpClientModule } from '@angular/common/http';
-import { LeafletMapPageComponent } from './leaflet-map-page/leaflet-map-page.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {HeaderComponent} from './header/header.component';
+import {MapPageComponent} from './map-page/map-page.component';
+import {PinsListPageComponent} from './pins-list-page/pins-list-page.component';
+import {GoogleMapsModule} from '@angular/google-maps';
+import {HttpClientModule} from '@angular/common/http';
+import {LeafletMapPageComponent} from './leaflet-map-page/leaflet-map-page.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MapPageComponent,
-    PinsListPageComponent,
-    LeafletMapPageComponent
-  ],
-  imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    GoogleMapsModule,
-    LeafletModule,
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        MapPageComponent,
+        PinsListPageComponent,
+        LeafletMapPageComponent
+    ],
+    imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatListModule,
+        MatIconModule,
+        GoogleMapsModule,
+        LeafletModule,
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        BrowserAnimationsModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
